8b4d4c9550cd5629a7cc35eeee5ece3d01aac9c4
backhorock <our-web@our-web.co.kr>
2023-04-18 12:47:17 +0900

    axios 설치

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,9 +41,15 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  axios: {
+    baseUrl: 'http://localhost:8000',
+    Credential: true,
   }
 }
